ca186bf0fcf51b2b5d53ee32a40eae2af0bc24a1
gab <gabrielgenarobg@gmail.com>
2026-04-18 20:15:41 -0500

    Creación Modulos

---
+++ turnosSystem/src/main/java/com/turnos/turnosSystem/TurnosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.turnos.turnosSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosSystemApplication.class, args);
+	}
+
+}
---
+++ turnosSystem/src/test/java/com/turnos/turnosSystem/TurnosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnos.turnosSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
